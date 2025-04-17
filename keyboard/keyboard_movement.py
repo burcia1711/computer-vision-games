292dbcba70c12690a71aea5fcdc91b1cd22516ac
Burcu <burcu.alakus@gmail.com>
2025-04-17 18:10:07 +0300

    keyboard continuity and fps

--- keyboard/keyboard_movement.py
+++ keyboard/keyboard_movement.py
@@ -13,24 +13,29 @@ player = pygame.image.load("avatar.png")
 player_coordinate = player.get_rect()
 player_coordinate.center = (250, 275)
 
+# Setting FPS
+FPS = 30
+clock = pygame.time.Clock()
+
 # Creating a game loop
 working = True
 while working:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             working = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                player_coordinate.y -= 5
-            elif event.key == pygame.K_DOWN:
-                player_coordinate.y += 5
-            elif event.key == pygame.K_LEFT:
-                player_coordinate.x -= 5
-            elif event.key == pygame.K_RIGHT:
-                player_coordinate.x += 5
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        player_coordinate.x-=5
+    elif key[pygame.K_RIGHT]:
+        player_coordinate.x+=5
+    elif key[pygame.K_UP]:
+        player_coordinate.y-=5
+    elif key[pygame.K_DOWN]:
+        player_coordinate.y+=5
 
     window.fill((0, 0, 0))
     window.blit(player, player_coordinate)
     pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
